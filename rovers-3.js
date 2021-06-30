07deed015023ff251b12eed4b170036e7a576425
juj624 <julienpenot@outlook.com>
2021-06-30 18:39:47 +0200

    create my funnction move for one

--- rovers-3.js
+++ rovers-3.js
@@ -39,9 +39,9 @@ function turnLeft(rover) {
     console.log(`Rover Direction: ${(rover).direction}`)
 }
 
-turnLeft(rover);
-turnLeft(rover);
-turnLeft(rover);
+// turnLeft(rover);
+// turnLeft(rover);
+// turnLeft(rover);
 
 
 function turnRight(rover) {
@@ -62,3 +62,51 @@ function turnRight(rover) {
     }
     console.log(`Rover Direction: ${rover.direction}`)
 }
+
+turnRight(rover);
+
+
+function moveForward(rover) {
+    console.log("moveForward was called");
+    switch (rover.direction) {
+
+        case rover.direction = "N":
+            if (rover.y === 0) {
+               console.log("you are out");
+            } else {
+                rover.y = rover.y-1;
+           }
+           break;
+
+        case rover.direction = "S":
+            if(rover.y === 9){
+                console.log("you are out");
+            }else{
+                rover.y = rover.y+1;
+            }
+            break;
+
+        case rover.direction = "W":
+            if(rover.x === 0){
+                console.log("you are out");
+            }else{
+                rover.x = rover.x-1;
+            }
+            break;
+        case rover.direction = "E":
+            if(rover.x === 9){
+                console.log("you are out")
+            }else{
+                rover.x = rover.x+1;
+            }
+            break;
+     }    
+     console.log(rover); 
+}
+
+moveForward(rover);
+
+
+// function piloteRover(rover){
+
+// }
